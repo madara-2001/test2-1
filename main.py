e5917edc28a55dc25339d749235cc61209c5649d
Andy-cya <120306358+Andy-cya@users.noreply.github.com>
2022-12-16 21:06:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -131,14 +131,5 @@ end_time = time.time()  # 程序结束时间
 run_time = end_time - start_time  # 程序的运行时间，单位为秒
 print(run_time)
 
-
-file_ = open(buffer, "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-
-st.markdown(
-    f'< img src="data:image/gif;base64,{data_url}" alt="gif">',
-    unsafe_allow_html=True,
-)
+st.image(gif)
 
